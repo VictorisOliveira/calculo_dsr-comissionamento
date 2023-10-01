95db9d651f8aa20b0a676be99281562cdc60d242
Victor I S Oliveira <70918081+VictorisOliveira@users.noreply.github.com>
2023-10-01 19:40:23 -0300

    Calculo_dsr
    Inovação

---
+++ calcular_comissao.py
@@ -0,0 +1,14 @@
+#Definir variavéis
+
+valor_venda = float(input("Digite o valor em vendas do consultor:R$ "))
+dias_trabalhado = int(input("Digite dias úteis trabalhados: "))
+sabado_mes = int(input("Quantos sabados teve o mês? "))
+valor_dia =  valor_venda / dias_trabalhado
+dsr = valor_dia * sabado_mes
+dsr_total = dsr + valor_venda
+dsr_total == int
+
+print(f"Seu comissionamento é R$",dsr_total)
+
+
+#feito por victorisOliveira/ES & marcelo-py/MA
